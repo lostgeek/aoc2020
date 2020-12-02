48ce67f356196a8d3d084ddc44d1988997263def
Oguz Han Asnaz <github@lostgeek.de>
2020-12-02 10:41:43 +0100

    Day 2 of AoC

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -1,5 +1,6 @@
 (ns aoc2020.core
   (:require [aoc2020.day01 :as day01])
+  (:require [aoc2020.day02 :as day02])
   (:gen-class))
 
 (defn -main
@@ -8,4 +9,5 @@
   (println
     (case (Integer/parseInt (first args))
       1 (day01/main)
+      2 (day02/main)
       "Day not found.")))
---
+++ src/aoc2020/day02.clj
@@ -0,0 +1,36 @@
+(ns aoc2020.day02
+  "AOC 2020 Day 2"
+  (:require [clojure.string :as string])
+  (:gen-class))
+
+(defn get-data
+  [f]
+  (letfn [(parse-line [line]
+            (let [match (re-matches #"(\d+)-(\d+) (\w): (.*)" line)]
+              {:req {:min (read-string (nth match 1))
+                     :max (read-string (nth match 2))
+                     :char (first (nth match 3))}
+               :pw (nth match 4)}))]
+        (-> f
+            slurp
+            string/trim
+            (#(string/split % #"\n"))
+            (#(map parse-line %)))))
+
+(defn validate-part1
+  [{:keys [req pw]}]
+  (let [occ (count (filter #(= (:char req) %) pw))]
+    (and (<= (:min req) occ)
+         (>= (:max req) occ))))
+
+(defn validate-part2
+  [{:keys [req pw]}]
+  (let [to-check [(get pw (dec (:min req)))
+                  (get pw (dec (:max req)))]]
+    (= 1 (count (filter #(= (:char req) %) to-check)))))
+
+(defn main
+  [& args]
+  (let [data (get-data "resources/day02.txt")]
+    (str "part 1: " (count (filter identity (map validate-part1 data))) "\n"
+         "part 2: " (count (filter identity (map validate-part2 data))))))

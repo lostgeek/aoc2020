3f82a07591e0cc9b2fcdaa52ccf7dc2fb9481ce2
Oguz Han Asnaz <github@lostgeek.de>
2020-12-14 15:59:11 +0100

    Day 13: second approach. Would take ~3h to run

--- src/aoc2020/day13.clj
+++ src/aoc2020/day13.clj
@@ -30,54 +30,28 @@
         bus (nth busses ind)]
     (* (- arrival (:time notes)) bus)))
 
-(defn create-timetables
-  [busses min-t]
-  (vec (for [bus busses]
-         (if (= :x bus)
-           (repeat :x)
-           (iterate (partial + bus)
-                    (* bus (long (Math/floor (/ min-t bus)))))))))
-
-(defn next-step
-  [b]
-  (loop [busses b
-         i 0]
-    (cond
-      (= i (count busses))
-      busses
-
-      (zero? i)
-      (recur (update busses i rest) (inc i))
-
-      (or (= :x (first (nth busses i)))
-              (> (first (nth busses i)) (first (first busses))))
-      (recur busses (inc i))
-
-      :else
-      (recur (update busses i rest) i))))
-
-(defn check
-  [busses]
-  (apply = (remove #(= :x %) (map (fn [x] (if (= :x (second x))
-                          :x
-                          (- (second x) (first x))))
-                  (map-indexed vector (map first busses))))))
-
+(defn create-timetable
+  [busses zerobus]
+  (remove #(= :x (second %)) (map-indexed vector busses)))
 
 (defn part2
   [notes min-t]
-  (loop [busses (create-timetables (:busses notes) min-t)
-         cnt 0]
-    (let [busses (next-step busses)]
+  (let [max-bus (apply max (remove #(= :x %) (:busses notes)))
+        busses (create-timetable (:busses notes) max-bus)
+        offset (first (first (filter #(= max-bus (second %)) busses)))]
+    (loop [t (- (* max-bus (long (Math/ceil (/ min-t max-bus))))
+                offset)
+           cnt 0]
       (when (zero? (mod cnt 100000))
-        (println cnt "--" (map first busses)))
-      (if (check busses)
-        (seq (map first busses))
-        (recur busses (inc cnt))))))
-
+        (println t))
+      (if (every? identity
+                  (for [[dt bus] busses]
+                    (= (mod (- dt) bus) (mod t bus))))
+        t
+        (recur (+ t max-bus) (inc cnt))))))
 (defn main
   [& args]
-  (let [data (get-data "resources/day13.txt")
-        min-t 100000000000000]
+  (let [data (get-data "resources/day13-test-4.txt")
+        min-t 0]
     (str "part 1: " (time (part1 data)) "\n"
          "part 2: " (time (part2 data min-t)))))

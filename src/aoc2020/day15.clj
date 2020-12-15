c9affec386d383b14c666c46407c990fb3f80503
Oguz Han Asnaz <github@lostgeek.de>
2020-12-15 15:06:07 +0100

    Cleanup day 15

--- src/aoc2020/day15.clj
+++ src/aoc2020/day15.clj
@@ -13,44 +13,26 @@
                  vec)]
     data))
 
-(defn next-num
-  [history]
-  (let [rev (reverse history)
-        n (first rev)
-        prev (filter #(= n (second %)) (map-indexed vector rev))]
-    (if (nil? (second prev))
-      0
-      (- (first (second prev)) (first (first prev))))))
-
-(defn next-step
-  [history]
-  (conj history (next-num history)))
-
-(defn part1
-  [data]
-  ; (seq (take 20 (nth (iterate next-step data) (- 2020 (count data))))))
-  (last (nth (iterate next-step data) (- 2020 (count data)))))
-
 (defn prepare-history
   [data]
-  (apply merge
-         (map-indexed (fn [ind x]
-                        {x {:prev nil :recent ind}})
-                      data)))
+  (into (hash-map) (apply merge
+                         (map-indexed (fn [ind x]
+                                        {x {:prev nil :recent ind}})
+                                      data))))
 
 (defn update-history
   [history n i]
   (let [entry (get history n)]
-    (merge history {n {:prev (:recent entry)
-                       :recent i}})))
+    (assoc history n {:prev (:recent entry)
+                      :recent i})))
 
-(defn part2
-  [data]
+(defn find-nth
+  [data n]
   (loop [history (prepare-history data)
          latest (last data)
          i (count data)]
-    ; (when (zero? (mod i 100000)) (println (float (/ i 30000000))))
-    (if (= i 30000000)
+    (when (zero? (mod i 1000000)) (println (float (/ i n))))
+    (if (= i n)
       latest
       (let [entry (get history latest)
             diff (if (nil? (:prev entry))
@@ -59,6 +41,14 @@
         (recur (update-history history diff i)
                diff
                (inc i))))))
+
+(defn part1
+  [data]
+  (find-nth data 2020))
+
+(defn part2
+  [data]
+  (find-nth data 30000000))
 
 (defn main
   [& args]

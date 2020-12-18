cf974c772a36533ff0e0d45661d2902a9cc080c3
Oguz Han Asnaz <github@lostgeek.de>
2020-12-18 16:22:21 +0100

    Adjusted profile for performance

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]]
-  :main ^:skip-aot aoc2020.core
+  :main aoc2020.core
+  :aot :all
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
